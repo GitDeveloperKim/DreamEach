531ba585817930b7134b1676cb7dec386c66c2b3
kim-youngseok <kim-youngseok@192.168.200.167>
2019-06-15 21:07:57 +0900

    add baekjoon 10869 src code

---
+++ DreamEach/src/baekjoon10869/Main.java
@@ -0,0 +1,21 @@
+package baekjoon10869;
+
+import java.util.Scanner;
+
+public class Main {
+	static int a;
+	static int b;
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner (System.in);
+		
+		a = in.nextInt();
+		b = in.nextInt();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+}
