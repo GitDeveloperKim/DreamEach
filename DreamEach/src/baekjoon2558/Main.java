f77f228b7c3fb4d223a88a942b8a155d1339f1c5
kim-youngseok <kim-youngseok@192.168.200.167>
2019-06-15 21:18:39 +0900

    add baekjoon 2558 src code

---
+++ DreamEach/src/baekjoon2558/Main.java
@@ -0,0 +1,17 @@
+package baekjoon2558;
+
+import java.util.Scanner;
+
+public class Main {
+	static int a;
+	static int b;
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner (System.in);
+		
+		a = in.nextInt();
+		b = in.nextInt();
+		
+		System.out.println( a+b );
+	}
+}
