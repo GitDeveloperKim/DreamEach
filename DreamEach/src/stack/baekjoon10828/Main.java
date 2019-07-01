b0f7bda09a6b5ba38f1a2af6a144aac412c61027
kim-youngseok <kim-youngseok@192.168.0.101>
2019-07-01 23:21:15 +0900

    add baekjoon 10828 src code type stack

---
+++ DreamEach/src/stack/baekjoon10828/Main.java
@@ -0,0 +1,56 @@
+package stack.baekjoon10828;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+	public static int N;
+	public static Stack<Integer> stack;
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner (System.in);
+		stack = new Stack<>();
+		
+		N = in.nextInt();
+		in.nextLine();
+		int i = 0;
+		while (i<N) {
+			String str = in.nextLine();
+			
+			switch (str) {
+			case "pop" :
+				if (stack.isEmpty()) {
+					System.out.println("-1");
+				} else {
+					System.out.println(stack.pop());
+				}
+				break;
+			case "size" :
+				System.out.println(stack.size());
+				break;
+			case "empty" :
+				if (stack.isEmpty()) {
+					System.out.println("1");
+				} else {
+					System.out.println("0");
+				}
+				break;
+			case "top" :
+				if (stack.isEmpty()) {
+					System.out.println("-1");
+				} else {
+					System.out.println(stack.peek());
+				}
+				break;
+			default :
+				String [] temp = str.split("\\s");
+				int x = Integer.parseInt(temp[1]);
+				stack.push(x);
+				break;
+			}
+			i++;
+		}
+		
+	}
+
+}
